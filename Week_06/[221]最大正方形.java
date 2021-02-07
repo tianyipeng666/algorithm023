f0a9a61eba62720c1735717024421e18f9966567
tianyipeng666 <506575523@qq.com>
2021-02-07 21:01:56 +0800

    sixth work

---
+++ Week_06/[221]最大正方形.java
@@ -0,0 +1,65 @@
+//在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"]
+//,["1","0","0","1","0"]]
+//输出：4
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：matrix = [["0","1"],["1","0"]]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：matrix = [["0"]]
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// m == matrix.length 
+// n == matrix[i].length 
+// 1 <= m, n <= 300 
+// matrix[i][j] 为 '0' 或 '1' 
+// 
+// Related Topics 动态规划 
+// 👍 660 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        int rows = grid.length, columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < columns; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < columns; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[rows - 1][columns - 1];
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
